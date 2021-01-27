d22e3d0c3f67cb1a287cea012c499bd620916f16
Kevin Johnson <kevin@coolkev.com>
2021-01-26 20:53:00 -0500

    Merge branch

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,49 +1,27 @@
 import React from "react";
-import "./App.css";
 
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { Songs } from "./scenes/songs-scene/Songs";
+import { Song } from "./scenes/song-scene/Song";
+import { Home } from "./scenes/home-scene/Home";
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/songs">Songs</Link>
-            </li>
-          </ul>
-        </nav>
-
-        <Switch>
-          <Route path="/songs">
-            <Songs />
-          </Route>
-          <Route path="/song/:songId">
-            <Song />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-      </div>
+    <Router basename={process.env.PUBLIC_URL}>
+      <Switch>
+        <Route path="/songs">
+          <Songs />
+        </Route>
+        <Route path="/song/:songId">
+          <Song />
+        </Route>
+        <Route path="/">
+          <Songs />
+        </Route>
+      </Switch>
     </Router>
   );
-}
-
-function Home() {
-  return <h2>Home</h2>;
-}
-
-function Songs() {
-  return <h2>About</h2>;
-}
-
-function Users() {
-  return <h2>Users</h2>;
 }
 
 export default App;
